cbea20679f14cfe71f59a8861e1acc0a97c1843c
Szymon Bugaj <felidadae@gmail.com>
2016-06-18 19:08:25 +0200

    Added database scheme; before testing;

--- .dev/tools.sh
+++ .dev/tools.sh
@@ -1,3 +1,20 @@
 #
 #	Here we put some useful bash functions
 #
+
+function test {
+	echo $k
+}
+
+function tabus__reload {
+	source .dev/tools.sh	
+}
+
+function tabus__shell {
+	python3 manage.py shell
+}
+
+function tabus__migrate {
+	python manage.py makemigrations main
+	python manage.py migrate
+}
--- main/models.py
+++ main/models.py
@@ -4,3 +4,24 @@ from django.db import models
 
 
 
+class User(models.Model):
+	username = models.CharField(max_length = 20)
+	def __str__(self):
+		return str(self.username)
+
+class Sentence(models.Model):
+	adding_date = models.DateTimeField()
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.CharField(max_length = 250)
+	def __str__(self):
+		return str(self.content + " " + self.owner.username )
+
+class Tag(models.Model):
+	tag = models.CharField(max_length = 20)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	def __str__(self):
+		return str(self.tag)	
+
+class TagSentence(models.Model):
+	tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+	sentence = models.ForeignKey(Sentence, on_delete=models.CASCADE)
