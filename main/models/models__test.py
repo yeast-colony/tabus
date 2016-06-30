bf5bfc84728e9fca5ac3da7851c959d7ef2c0924
Szymon Bugaj <felidadae@gmail.com>
2016-07-01 00:04:08 +0200

    Minor changes in models__test.py

--- main/models/models__test.py
+++ main/models/models__test.py
@@ -9,14 +9,17 @@ import main.models.modelsOps as modelsOps
 import main.models.testingUtils as testingUtils
 
 
+
 class TestCase__models(TestCase):
     def setUp(self):
-        modelsOps.addEntries(testingUtils.test__entries)
+        pass
 
     def tearDown(self):
         pass
 
     def test__addEntry(self):
+        modelsOps.addEntries(testingUtils.test__entries)
+
         self.assertTrue(len(User.objects.all()) == 2)
         self.assertTrue(len(Sentence.objects.all()) == 4)
         self.assertEqual([str(db.username) for db in
@@ -24,23 +27,33 @@ class TestCase__models(TestCase):
                          testingUtils.test__userFromEntries)
 
     def test__addUser(self):
-        for user in testingUtils.test__users:
-            modelsOps.addUser(user)
-        self.assertTrue(len(User.objects.filter(username__in=testingUtils.test__users)))
-        for user in testingUtils.test__users:  # doing it twice to check if they are unique
-            modelsOps.addUser(user)
-        self.assertTrue(len(User.objects.filter(username__in=testingUtils.test__users)))
+        """
+            check if we add twice users,
+            we have only once added users
+        """
+        for i in range(2):
+            for user in testingUtils.test__users:
+                modelsOps.addUser(user)
+        
+        self.assertEqual([str(db.username) for db in
+                         User.objects.all()].sort(),
+                         testingUtils.test__users.sort())
 
     def test__addSentence(self):
+        modelsOps.addEntries(testingUtils.test__entries)
+
         userDB = User.objects.get(username=testingUtils.test__users[0])
         for sentence in testingUtils.test__sentences:
-            modelsOps.addSentence(sentence, userDB,
-                                  adding_date=timezone.now())
-        self.assertTrue(len(Sentence.objects.filter(content__in=testingUtils.test__sentences)))
+            modelsOps.addSentence(
+                sentence, userDB, adding_date=timezone.now())
+        self.assertTrue(len(Sentence.objects.filter(
+            content__in=testingUtils.test__sentences)))
         for sentence in testingUtils.test__sentences:
-            modelsOps.addSentence(sentence, userDB,
-                                  adding_date=timezone.now())
-        self.assertTrue(len(Sentence.objects.filter(content__in=testingUtils.test__sentences)))
+            modelsOps.addSentence(
+                sentence, userDB, adding_date=timezone.now())
+        self.assertTrue(
+            len(Sentence.objects.filter(
+                content__in=testingUtils.test__sentences)))
 
     def test__search(self):
         """
@@ -52,15 +65,16 @@ class TestCase__models(TestCase):
 		3)** what we are expecting when user doesn't have any sentences
 		"""
 
-    # (1)
+        modelsOps.addEntries(testingUtils.test__entries)
 
+
+        # (1)
         for test__entry in testingUtils.test__entries:
             (username, sentence, tags) = test__entry
             self.assertTrue(sentence in modelsOps.search(tags,
                             username))
 
-    # (2)
-
+        # (2)
         self.assertEqual(modelsOps.search(['wazne'], 'Agata'),
                          testingUtils.test__sentenes__agata_wazne)
 
